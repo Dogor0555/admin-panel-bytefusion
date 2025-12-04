ec68ee1951965fcb351551b3d29be5085cc32fd3
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-12-04 14:32:03 -0600

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className="text-black min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           {/* Logo y Título */}
